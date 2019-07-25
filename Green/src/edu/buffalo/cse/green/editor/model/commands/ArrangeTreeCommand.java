49f26d71fe1b3b11a9323be7ac8b13f805f34109
Luize Steina <luize0steina@gmail.com>
2019-07-25 15:10:54 +0300

    Tree Layout DONE

--- Green/src/edu/buffalo/cse/green/editor/model/commands/ArrangeTreeCommand.java
+++ Green/src/edu/buffalo/cse/green/editor/model/commands/ArrangeTreeCommand.java
@@ -85,12 +85,13 @@ public class ArrangeTreeCommand extends Command {
 		RootModel root = editor.getRootModel();
 		List<AbstractModel> children = root.getChildren();
 		int maxLevel = getMaxLevel(children);
+
 		// tops contains the classes that do not extend any other class shown in the
 		// diagram
 		List<TypeModel> tops = new ArrayList<>();
-		List<TypeModel> ends = new ArrayList<>();
-		// nextX provides spacing between trees on the x axis
-		int nextX = 0;
+
+		// xGlobalOffset provides spacing between drawn trees
+		int xGlobalOffset = 0;
 
 		for (AbstractModel mod : children) {
 			if (mod instanceof TypeModel) {
@@ -102,14 +103,12 @@ public class ArrangeTreeCommand extends Command {
 				}
 			}
 		}
-		System.out.println(tops);
 
 		// draws a tree for each class in tops
 		for (AbstractModel t : tops) {
 			if (t instanceof TypeModel) {
 
-				// nextX = drawTree((TypeModel) t, maxLevel, nextX);
-				drawT((TypeModel) t, maxLevel);
+				xGlobalOffset = drawTreeCentered((TypeModel) t, maxLevel, xGlobalOffset);
 			}
 		}
 
@@ -127,211 +126,157 @@ public class ArrangeTreeCommand extends Command {
 	}
 
 	/**
+	 * Draws a tree hierarchy and returns int value that shows the right bound of
+	 * the layout where this tree ends. This value is used for the next tree to be
+	 * drawn next to the one already drawn.
 	 * 
 	 * @param top
-	 *            - the parent class (root) of the tree
-	 * @param max
-	 *            - maximum depth
-	 * @param prevX
-	 *            - the tree will add a space between this coordinate and then place
-	 *            itself there
-	 * @return the furthest x coordinate the tree occupies
+	 *            - the root model for tree
+	 * @param maxLayoutLevel
+	 *            - max level of the hierarchy
+	 * @param xGlobalOffset
+	 *            - x coordinate from which to start drawing
+	 * @return int
 	 */
-	public int drawTree(TypeModel top, int max, int prevX) {
+	public int drawTreeCentered(TypeModel top, int maxLayoutLevel, int xGlobalOffset) {
 		DiagramEditor editor = DiagramEditor.getActiveEditor();
 		List<AbstractModel> allModels = getAllModels(top);
 
-		// xPos provides spacing on the x axis between the graphical elements
-		int[] xPos = new int[max + 1];
-		Arrays.fill(xPos, (prevX + 100));
-		for (int el : xPos) {
-
-		}
-		int nextX = 0;
-
-		/*
-		 * Y coordinates for the objects are determined by their levels. These levels
-		 * make the tree structure more comprehensible, as classes are placed under
-		 * their parent class.
-		 */
-
-		for (AbstractModel m : allModels) {
-			if (m instanceof TypeModel) {
-				int lvl = getLevel((TypeModel) m);
-				int level = max - lvl;
-				int childrenCount, parentCount;
-				// System.out.println("lvl: " + lvl);
-				//
-				//
-				// System.out.println("AbstractModel: " + m);
-				childrenCount = ((TypeModel) m).getIncomingEdges().size();
-				parentCount = ((TypeModel) m).getOutgoingEdges().size();
-				// System.out.println("Children: " + childrenCount);
-				// System.out.println("Parents: " + parentCount);
-
-				m.setSize(200, 100);
-
-				m.setLocation(xPos[level], (level * (-150)));
-
-				xPos[level] += 250;
-
-				// System.out.println("level: " + level);
-				// System.out.println();
-			}
-		}
-
-		for (int i = 0; i < xPos.length; i++) {
-			if (xPos[i] > nextX)
-				nextX = xPos[i];
-
-		}
-
-		return nextX;
-	}
-
-	public void drawT(TypeModel top, int maxLayoutLevel) {
-		DiagramEditor editor = DiagramEditor.getActiveEditor();
-		List<AbstractModel> allModels = getAllModels(top);
-		int[] xGlobalOffset = new int[maxLayoutLevel + 1];
-		int xGOffset = 0;
-		Arrays.fill(xGlobalOffset, 0);
 		TypeModel prevModel = null, currModel;
-		
-		
+
 		for (AbstractModel m : allModels) {
-			System.out.println("AbstractModel m: " + m);
-			
+
 			if (m instanceof TypeModel) {
-				int lvl = getLevel((TypeModel) m);
-				currModel = (TypeModel)m;
+				int level = getLevel((TypeModel) m);
+				currModel = (TypeModel) m;
 				int currModelIncomingEdges = currModel.getIncomingEdges().size();
-				System.out.println(currModel + "Incoming edges: " + currModelIncomingEdges);
-				
+				int lvl = maxLayoutLevel - level;
+
 				// Set size for each TypeModel
 				m.setSize(200, 100);
-				System.out.println("prevModParent: " + getParent(prevModel) + ", currModParent: " + getParent(currModel));
+
 				// Check if previous TypeModels parent is the same as the current ones
-				if(getParent(prevModel) == getParent(currModel)) {
-						System.out.println("(" + (prevModel.getBounds().x + 250) + ";" + prevModel.getBounds().y + ")");
-						
-						// If it is, set its location next to his brother with 50 pixels white space on the same level
-						currModel.setLocation(prevModel.getBounds().x + 250, prevModel.getBounds().y);
-						
-						// Remember TypeModels x axis that is the most to the right in the layout
-						if(currModel.getBounds().x >= xGOffset) {
-							System.out.println("xOffset: " + xGOffset);
-							xGOffset = currModel.getBounds().x + 250;
-							System.out.println("xOffset: " + xGOffset);
-							
-						
+				// prevModel != null is because of the first TypeModel in the list, it has no
+				// previous model recorded
+				if (getParentTypeModel(prevModel) == getParentTypeModel(currModel) && prevModel != null) {
+
+					// If it is, set its location next to his brother with 50 pixels white space on
+					// the same level
+					currModel.setLocation(xGlobalOffset, prevModel.getBounds().y);
+
+					// Remember TypeModels x axis that is the most to the right in the layout
+					if (currModel.getBounds().x >= xGlobalOffset) {
+
+						xGlobalOffset = currModel.getBounds().x + 250;
+
 					}
-					
-				// If TypeModel has more than 2 children, it needs to be drawn on his own level, but so that it would be in the middle of his children regarding x axis
-				} else if(currModelIncomingEdges > 1){
-					TypeModel [] childrenArray = new TypeModel[currModelIncomingEdges];
-	
+
+					// If TypeModel has more than 2 children, it needs to be drawn on his own level,
+					// but so that it would be in the middle of his children regarding x axis
+				} else if (currModelIncomingEdges > 1) {
+					TypeModel[] childrenArray = new TypeModel[currModelIncomingEdges];
+
 					// Array holds currentModel children TypeModels
 					childrenArray = getChildrenTypeModels(currModel);
-					
+
 					// x axis for the most left child
 					int xLeft = childrenArray[0].getBounds().x;
-					
+
 					// x axis for the most right child
 					int xRight = childrenArray[childrenArray.length - 1].getBounds().x;
-					
+
 					// 200 is added, because 200 is width of the box
 					// divided with 2, because parent needs to be in the middle of the children
-					// minus 100 because the box x coordinate is for upper left corner, so if we 
-					//	put this corner int the middle, we need to push it back to the left 200/2
-					currModel.setLocation(xLeft + ((xRight - xLeft + 200)/2)-100, lvl * (-150));
-					
-				// If TypeModel has 1 children, it needs to be drawn right on top of it
-				}else if (currModelIncomingEdges == 1){
+					// minus 100 because the box x coordinate is for upper left corner, so if we
+					// put this corner int the middle, we need to push it back to the left 200/2
+					currModel.setLocation(xLeft + ((xRight - xLeft + 200) / 2) - 100, lvl * (-150));
+
+					// If TypeModel has 1 children, it needs to be drawn right on top of it
+				} else if (currModelIncomingEdges == 1) {
 					currModel.setLocation(prevModel.getBounds().x, lvl * (-150));
-					
-					if(currModel.getBounds().x >= xGOffset) {
-						System.out.println("xOffset: " + xGOffset);
-						xGOffset = currModel.getBounds().x + 250;
-						System.out.println("xOffset: " + xGOffset);
-						
+
+					if (currModel.getBounds().x >= xGlobalOffset) {
+
+						xGlobalOffset = currModel.getBounds().x + 250;
+
 					}
-				
-				// If TypeModel has no children
-				}else if(currModelIncomingEdges == 0){
-					//System.out.println("(" + (prevModel.getBounds().x + 250) + ";" + prevModel.getBounds().y + ")");
-					
-					currModel.setLocation(xGOffset, lvl * (-150));
-					if(currModel.getBounds().x >= xGOffset) {
-						System.out.println("xOffset: " + xGOffset);
-						xGOffset = currModel.getBounds().x + 250;
-						System.out.println("xOffset: " + xGOffset);
-						
+
+					// If TypeModel has no children
+				} else if (currModelIncomingEdges == 0) {
+
+					currModel.setLocation(xGlobalOffset, lvl * (-150));
+					if (currModel.getBounds().x >= xGlobalOffset) {
+
+						xGlobalOffset = currModel.getBounds().x + 250;
+
 					}
-					
+
 				} else {
 					currModel.setLocation(prevModel.getBounds().x + 250, lvl * (-150));
-					if(xGOffset < currModel.getBounds().x) {
-						xGOffset = currModel.getBounds().x + 250;
-						
+					if (xGlobalOffset < currModel.getBounds().x) {
+						xGlobalOffset = currModel.getBounds().x + 250;
+
+					}
+
 				}
-					
-//			for(int o = 0; o < xLeftBound.length; o++) {
-//				System.out.println("xLeftBound[" + o + "]: " + xLeftBound[o]);
-//				
-			}
-			
-			prevModel = (TypeModel) m;
-			System.out.println();
+
+				// Sets previous model to this model, so that the next TypeModel could use this
+				// TypeModel as reference for calculations
+				prevModel = (TypeModel) m;
 			}
 		}
+		// Add 200 pixels to make space between trees with different roots bigger
+		return xGlobalOffset + 200;
 	}
-	
+
 	/**
-	 * Gets the parent TypeModel object for given TypeModel. If this method is called for null TypeModel, return will be null
+	 * Gets the parent TypeModel object for given TypeModel. If this method is
+	 * called for null TypeModel, return will be null
 	 * 
 	 * @param TypeModel
 	 * @return TypeModel of passed TypeModel. Null if has no parents.
 	 */
-	public TypeModel getParent (TypeModel currModel) {
+	public TypeModel getParentTypeModel(TypeModel currModel) {
 		TypeModel parentModel = null;
-		
-		if(currModel == null) {
+
+		if (currModel == null) {
 			return null;
 		}
-		
+
 		Set<RelationshipModel> outgoingBound;
-		
+
 		outgoingBound = ((TypeModel) currModel).getOutgoingEdges();
-		for(RelationshipModel e : outgoingBound) {
+		for (RelationshipModel e : outgoingBound) {
 			parentModel = e.getTargetModel();
 		}
-		
+
 		return parentModel;
 	}
 
 	/**
-	 * Gets a TypeModel array which holds all the children TypeModels for given TypeModel
+	 * Gets a TypeModel array which holds all the children TypeModels for given
+	 * TypeModel
 	 * 
 	 * @param TypeModel
 	 * @return TypeModel array
 	 */
-	public TypeModel[] getChildrenTypeModels (TypeModel currModel) {
+	public TypeModel[] getChildrenTypeModels(TypeModel currModel) {
 		int i = currModel.getIncomingEdges().size();
-		TypeModel [] childrenArray = new TypeModel[i];
+		TypeModel[] childrenArray = new TypeModel[i];
 		Set<RelationshipModel> incomingBounds;
+
 		incomingBounds = currModel.getIncomingEdges();
 		i = 0;
-		System.out.println("Children: ");
-		for(RelationshipModel incBound : incomingBounds) {
+
+		for (RelationshipModel incBound : incomingBounds) {
 			childrenArray[i] = incBound.getSourceModel();
-			System.out.println(i + ") " + childrenArray[i]);
 			i++;
-			
+
 		}
-		
+
 		return childrenArray;
 	}
-	
+
 	/**
 	 * 
 	 * @param top
